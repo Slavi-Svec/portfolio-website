0a3246de1b2f63b7cb3c8257501c1e7f16618dfe
izaak svec <slavi.svec@gmail.com>
2020-05-14 16:39:09 +1000

    Added JS image

--- portfolio/src/Pages/HomePage/HomePage.jsx
+++ portfolio/src/Pages/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ import Navigation from '../../Components/Navigation/Navigation'
 import AvatarLogo from '../../Assets/AvatarLogo.png'
 import AvatarLogo2 from '../../Assets/AvatarLogo2.png'
 import AvatarLogo3  from '../../Assets/AvatarLogo3.png'
+import JSLogo from '../../Assets/JSLogo.png'
 import Image from '../../Components/Image/Image'
 import Text from '../../Components/Text/Text'
 import Footer from '../../Components/Footer/Footer'
@@ -39,10 +40,15 @@ const HomePage = () => {
                     variant="p"
                 />
                 <section className="home__Work-details">
-                <Text text="What i can do." variant="h2" />
+                <Text text="More about me." variant="h2" />
                 </section>
                 <Image className="home__logo" src={AvatarLogo3} />
+                <Image className="home__logo-JS" src={JSLogo} />
+                <Text text="What i can do" variant="h3" />
+
                 <Image className="home__logo" src={AvatarLogo2} />
+                <Text text="And when im not coding....." variant="h3" />
+
             </section>
             <Footer />
          </section>
